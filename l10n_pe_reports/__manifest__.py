55b6568e998762555dede4ce89ff22e693caf23e
Jesus Fabian Cubas <jfabian@superpet.pe>
2022-06-15 19:21:46 -0500

    upload addon letra

--- l10n_pe_reports/__manifest__.py
+++ l10n_pe_reports/__manifest__.py
@@ -1,5 +1,9 @@
 {
     "name": "Reportes de prueba",
     "version": "15.0.0",
-    "depends": ["account", "sale"]
+    "depends": ["account", "sale"],
+    "data": [
+        "views/letras_views.xml",
+        "security/ir.model.access.csv"
+    ]
 }
