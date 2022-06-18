319c8911c98d06c747839411568ade168e800f92
Jesus Fabian Cubas <jfabian@superpet.pe>
2022-06-17 19:29:34 -0500

    custom xml form

--- prueba_addon/models/letra_invoice.py
+++ prueba_addon/models/letra_invoice.py
@@ -12,7 +12,6 @@ class LetraInvoice(models.Model):
     name = fields.Char(size=100, string='Nombre')
     type_letra = fields.Selection([('descuento', 'Letra en descuento'), ('cartera', 'Letra en Cartera')])
     invoice_id = fields.Many2one(comodel_name='account.move', string='Factura')
-    invoice_draft_id = fields.Many2one(comodel_name='account.move', string='Factura borrador', domain="[('state', 'in', ['draft']), ('move_type', '=', 'out_invoice')]")
     amount_total = fields.Float(required=False, string='Valor de la letra')
     created_by = fields.Many2one(comodel_name='res.users', string='Vendedor', default=lambda self: self.env.user.id, index=1)
 
